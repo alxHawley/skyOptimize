a017659c25895fdc28bd49c9d61caa79f5cd5b94
Alex Hawley <102092963+alxHawley@users.noreply.github.com>
2023-08-19 15:07:31 -0700

    initial implementation

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"html/template"
+	"net/http"
+	"skyOptimize/moondata"
+)
+
+func main() {
+	http.HandleFunc("/moondata", moonDataHandler)
+	http.ListenAndServe(":8080", nil)
+}
+
+func moonDataHandler(w http.ResponseWriter, r *http.Request) {
+	date := "2022-08-16"
+	lat := "47.6062"
+	long := "-122.3321"
+	tz := "-7.0"
+	dst := "1"
+
+	// Call moonData function to get MoonData struct
+	moonData, err := moondata.GetMoonData(date, lat, long, tz, dst)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Parse HTML template file
+	tmpl, err := template.ParseFiles("skyOptimize.html")
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	// Execute HTML template with MoonData struct as data context
+	err = tmpl.Execute(w, moonData.Properties.Data)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+}
---
+++ moondata/moondata.go
@@ -0,0 +1,51 @@
+package moondata
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+// Struct to handle the data from the API
+type MoonData struct {
+	Geometry struct {
+		Coordinates []float64 `json:"coordinates"`
+		Type        string    `json:"type"`
+	} `json:"geometry"`
+	Properties struct {
+		Data struct {
+			CurPhase  string `json:"curphase"`
+			FracIllum string `json:"fracillum"`
+			MoonData  []struct {
+				Phen string `json:"phen"`
+				Time string `json:"time"`
+			} `json:"moondata"`
+		} `json:"data"`
+	} `json:"properties"`
+	Type string `json:"type"`
+}
+
+func GetMoonData(date string, lat string, long string, tz string, dst string) (MoonData, error) {
+	url := fmt.Sprintf("https://aa.usno.navy.mil/api/rstt/oneday?date=%s&coords=%s,%s&tz=%s&dst=%s", date, lat, long, tz, dst)
+	// Make HTTP request to get JSON response object
+	resp, err := http.Get(url)
+	if err != nil {
+		// Handle error
+	}
+	defer resp.Body.Close()
+
+	// Decode JSON response object into MoonData struct
+	var moonData MoonData
+	err = json.NewDecoder(resp.Body).Decode(&moonData)
+	if err != nil {
+		// Handle error
+	}
+
+	// Print out some fields from the MoonData struct
+	fmt.Println("Current Phase:", moonData.Properties.Data.CurPhase)
+	fmt.Println("Fraction Illuminated:", moonData.Properties.Data.FracIllum)
+	fmt.Println("Moon Rise Time:", moonData.Properties.Data.MoonData[0].Time)
+	fmt.Println("Moon Set Time:", moonData.Properties.Data.MoonData[2].Time)
+
+	return moonData, nil
+}
